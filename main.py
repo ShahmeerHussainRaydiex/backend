1a3a87522859e166e9acca05f6c2e76ff3e715b8
ShahmeerHussainRaydiex <162441724+ShahmeerHussainRaydiex@users.noreply.github.com>
2024-05-09 15:01:45 +0500

    added Pixabay and modifed gpt response

--- main.py
+++ main.py
@@ -1,86 +1,58 @@
 from pathlib import Path
 import uvicorn
-from fastapi import FastAPI, HTTPException, Header, UploadFile, File, Form
+from fastapi import FastAPI, HTTPException, UploadFile, File, Query
 import requests
-from pypexels import PyPexels
-from helper import video_to_base64
-import json
 from dotenv import load_dotenv
 import os
 from uuid import uuid4
 from fastapi.responses import FileResponse
+from pydub.utils import mediainfo
 
 app = FastAPI()
 load_dotenv()
 
 client = ""
 api_key = os.environ.get("OPENAI_API_KEY")
+PIXABAY_API_KEY = os.environ.get("PIXABAY_API_KEY")
 
 if api_key is not None:
     from openai import OpenAI
 
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-
-@app.get("/check")
-async def root():
-    import ffmpeg
-
-    input_background = ffmpeg.input('background.mp4')
-    input_color = ffmpeg.input('test.mp4')
-    input_alpha = ffmpeg.input('test.mp4')
-
-    coloralpha = ffmpeg.filter([input_color, input_alpha], 'alphamerge')
-
-    background_resized = \
-        input_background.filter('scale2ref', 'main_w*max(iw/main_w, ih/main_h)', 'main_h*max(iw/main_w, ih/main_h)')[0]
-    overlay = ffmpeg.overlay(background_resized, coloralpha, shortest=1, x='main_w/2-overlay_w/2', y='main_h-overlay_h')
-
-    darset = overlay.filter('setdar', 'dar=a').filter('pad', 'ceil(iw/2)*2', 'ceil(ih/2)*2')[0]
-    output = darset
-
-    output = output.output('output.mp4', vcodec='libx264', crf=17, pix_fmt='yuv420p', strict='experimental',
-                           acodec='copy')
-
-    output.run()
-
-    output.run()
-
-    return {f"message": f"Hello World"}
-
-
 API_KEY = os.getenv("PEXEL")
 
 
 @app.get("/search/images")
 async def search_images(query: str):
-    headers = {
-        "Authorization": f"{API_KEY}"
-    }
+    headers = {"Authorization": f"{API_KEY}"}
 
     # Make a request to the Pexels API
-    response = requests.get(f"https://api.pexels.com/v1/search?query={query}", headers=headers)
+    response = requests.get(
+        f"https://api.pexels.com/v1/search?query={query}", headers=headers
+    )
 
     if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail="Pexels API request failed")
+        raise HTTPException(
+            status_code=response.status_code, detail="Pexels API request failed"
+        )
     response = response.json()
-    image_url = response['photos'][0]['url']
+    image_url = response["photos"][0]["url"]
 
     return image_url
 
 
 async def translate_text(text, language):
-
     prompt = f"Translate the following English text into {language}: {text}"
     story_response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": prompt}
-        ],
-        max_tokens=150
+        messages=[{"role": "system", "content": prompt}],
+        max_tokens=150,
     )
-    translate_text = story_response.choices[0].message.content
-    return translate_text
+    translated_text = story_response.choices[0].message.content
+    return translated_text
+
+
 @app.get("/title")
 async def title(text):
     # Define the parameters for the translation request
@@ -88,24 +60,23 @@ async def title(text):
     prompt = f"Generate a title for following story {text}"
     story_response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": prompt}
-        ],
-        max_tokens=150
+        messages=[{"role": "system", "content": prompt}],
+        max_tokens=150,
     )
-    title = story_response.choices[0].message.content
-    return title
+    prompt_title = story_response.choices[0].message.content
+    return prompt_title
+
 
 def search_videos(queries):
-    headers = {
-        "Authorization": f"{API_KEY}"
-    }
+    headers = {"Authorization": f"{API_KEY}"}
     urls = []
     # Make a request to the Pexels API
     for query in queries:
         try:
-            response = requests.get(f"https://api.pexels.com/videos/search?query={query}&per_page=1",
-                                    headers=headers)
+            response = requests.get(
+                f"https://api.pexels.com/videos/search?query={query}&per_page=1",
+                headers=headers,
+            )
         except Exception as e:
             return {"error": e}
 
@@ -117,74 +88,18 @@ def search_videos(queries):
     return urls
 
 
-@app.get("/search/test")
-async def search_video(query: str, num_page: int = 1, per_page: int = 1):
-    headers = {
-        "Authorization": API_KEY
-    }
-    py_pexel = PyPexels(api_key=API_KEY)
-    search_videos_page = py_pexel.videos_search(query=query, per_page=per_page)
-    for video in search_videos_page.entries:
-        print(video.id, video.user.get('name'), video.url)
-
-        url = f"https://api.pexels.com/videos/videos/{video.id}"
-        response = requests.get(url, headers=headers)
-        data_url = 'https://www.pexels.com/video/' + str(video.id) + '/download'
-        r = requests.get(data_url)
-        print(r.headers.get('content-type'))
-        with open(f'{video.id}.mp4', 'wb') as outfile:
-            outfile.write(r.content)
-    return {f"message": f"Downloaded videos"}
-
-
-@app.get("/video/video")
-async def search_videos_stable(prompt: str):
-    url = "https://modelslab.com/api/v6/video/video2video"
-    video_path = "8625246.mp4"  # Provide the path to your video file
-    base64_video = video_to_base64(video_path)
-    print(base64_video)
-
-    payload = json.dumps({
-        "key": os.getenv("STABLE_DIFFUSION"),
-        "model_id": "midjourney",
-        "prompt": prompt,
-        "negative_prompt": "low quality",
-        "init_video": base64_video,
-        "height": 512,
-        "width": 512,
-        "num_frames": 16,
-        "num_inference_steps": 20,
-        "guidance_scale": 7,
-        "strength": 0.7,
-        "base64": True,
-        "webhook": None,
-        "track_id": None
-    })
-
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    print(response.text)
-    return response.json()
-
-
 @app.get("/convert_text_to_speech/")
 async def convert_text_to_speech(text: str, voice: str = "onyx", language=""):
     if language != "":
         text = await translate_text(text, language)
     try:
-        response = client.audio.speech.create(
-            model="tts-1",
-            voice=voice,
-            input=text
-        )
+        response = client.audio.speech.create(model="tts-1", voice=voice, input=text)
         unique_filename = f"speech_{uuid4()}.mp3"
         speech_file_path = Path(__file__).parent / unique_filename
         response.stream_to_file(speech_file_path)
-        return FileResponse(unique_filename)
+        audio_duration = mediainfo(speech_file_path)['duration']
+        headers = {"X-Audio-Duration": str(audio_duration)}
+        return FileResponse(unique_filename, headers=headers)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -194,8 +109,7 @@ async def transcribe_audio(audio_file: UploadFile = File(...)):
     try:
         with audio_file.file as file:
             transcription = client.audio.transcriptions.create(
-                model="whisper-1",
-                file=file
+                model="whisper-1", file=file
             )
         return {"transcription": transcription.text}
     except Exception as e:
@@ -204,56 +118,60 @@ async def transcribe_audio(audio_file: UploadFile = File(...)):
 
 async def generate_story(prompt: str = "You're tasked with writing a story script "):
     try:
-        prompt =f" generate a story on topic {prompt}"
+        prompt = f" generate a story on topic {prompt}"
         story_response = client.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": prompt}
-            ],
-            max_tokens=150
+            messages=[{"role": "system", "content": prompt}],
+            max_tokens=150,
         )
-        story = story_response.choices[0].message.content.replace('\n', ' ')
-        story = story.replace('\\', '')
+        story = story_response.choices[0].message.content.replace("\n", " ")
+        story = story.replace("\\", "")
 
         keyword_response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
                     "role": "system",
-                    "content": "You will be provided with a block of text, and your task is to extract sentences of 3 to 4 words which can help to search videos on pexel."
+                    "content": "You will be provided with a block of text, and your task is to extract keywords "
+                               "separated by commas ",
                 },
-                {
-                    "role": "user",
-                    "content": story
-                }
+                {"role": "user", "content": story},
             ],
             temperature=0.5,
             max_tokens=64,
-            top_p=1
+            top_p=1,
         )
-        # urls = search_videos()
-        queries = keyword_response.choices[0].message.content.split('\n')
-        headers = {
-            "Authorization": f"{API_KEY}"
-        }
+        queries = keyword_response.choices[0].message.content.split(",")
+        headers = {"Authorization": f"{API_KEY}"}
         urls = []
         # Make a request to the Pexels API
+        print(queries)
+        print("________________________________________________________________________________--")
+        # queries = queries[0].split('-')
+        print(queries)
         for query in queries:
-            try:
-                response = requests.get(
-                    f"https://api.pexels.com/videos/search?query={query}&per_page=1&orientation=landscape&size=small",
-                    headers=headers)
-            except Exception as e:
-                return {"error": e}
+            print(query)
+            pixabay_url = await get_videos(query)
+            if pixabay_url["videos"] != []:
+                urls.append(pixabay_url["videos"][0])
+            else:
+                try:
+                    response = requests.get(
+                        f"https://api.pexels.com/videos/search?query={query}&per_page=1&orientation=landscape&size=small",
+                        headers=headers,
+                    )
+                except Exception as e:
+                    return {"error": e}
 
-            if response.status_code != 200:
-                continue
-            response = response.json()
-            for video in response['videos'][0]['video_files']:
-                if video['height'] == 720 and video['width'] == 1280:
-                    urls.append(video['link'])
-                    break
-        return {"urls": urls, "story": story}
+                if response.status_code != 200:
+                    continue
+                response = response.json()
+                if response["videos"] != []:
+                    for video in response["videos"][0]["video_files"]:
+                        if video["height"] == 720 and video["width"] == 1280:
+                            urls.append(video["link"])
+                            break
+        return {"urls": urls, "story": story, "keywords": queries}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -283,27 +201,23 @@ async def story_generation(prompt: str):
     return {"response ": await generate_story(prompt)}
 
 
-@app.get("/convert_text_to_music/")
-async def convert_text_to_music(text: str):
-    import requests
+async def get_videos(query: str = Query(...)):
+    """
+    Retrieve videos from Pixabay.
+    """
 
-    url = "https://api.tryleap.ai/api/v1/music"
+    pixabay_url = (
+        f"https://pixabay.com/api/videos/?key={PIXABAY_API_KEY}&q={query}"
+    )
 
-    payload = {
-        "prompt": "An electronic music soundtrack with a trumpet solo",
-        "mode": "melody",
-        "duration": 28
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "X-Api-Key": "le_62f9c378_MH3yuPlrw4TCf5IRK57ANJGY"
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    print(response.text)
-    return response.json()
+    try:
+        response = requests.get(pixabay_url)
+        response.raise_for_status()
+        data = response.json()
+        video_urls = [item["videos"]["medium"]["url"] for item in data["hits"]]
+        return {"videos": video_urls}
+    except requests.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Failed to fetch videos: {str(e)}")
 
 
 if __name__ == "__main__":
